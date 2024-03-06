c8485454f94b575fe47b22f0f03c6585961d955e
Justin <dong135790@gmail.com>
2024-03-05 21:13:15 -0500

    updating database

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : 'postgresql-reticulated-41517',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
